6c89637db8d6944518565d85108c49dbe0be108f
robertisaac <robert.isaac@live.com>
2019-05-28 21:35:41 +0200

    fix double important SoundModule you can only import it lazy loaded from app-routing or normally from app.module but you can't import it from both

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,19 +3,15 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
-import { SoundModule } from './sound/sound.module';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    SoundModule,
-    
   ],
   providers: [],
   bootstrap: [AppComponent]
